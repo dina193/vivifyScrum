69758133cf0b089d5f2e45dcc050ff8b52b245a6
dina193 <bolta.dina@gmail.com>
2022-05-25 22:41:26 +0200

    vs second homework

--- cypress/integration/organization.spec.js
+++ cypress/integration/organization.spec.js
@@ -7,62 +7,109 @@ import sidebar from "../pages/sidebar.json";
 import orgPage from "../pages/organizationPage.json";
 import header from "../pages/header.json";
 
+
 describe("Organization CRUD", () => {
-    it("Visit login page and login", () => {
+    before(() => {
         cy.visit("/login");
         cy.get(loginPage.emailInput).type(data.user.email);
         cy.get(loginPage.passwordInput).type(data.user.password);
-        cy.get(loginPage.loginBtn).click();
+        cy.get(loginPage.loginBtn)
+            .should("be.visible")
+            .click();
+        cy.url().should("contain", "/my-organizations");
+    });
+
+    after(() => {
+        cy.get(sidebar.mainSidebar.myAccountAvatarImg)
+            .should("be.visible")
+            .click();
+        cy.get(sidebar.sideMenu.accountOptions.profileBtn)
+            .should("be.visible")
+            .click();
+        cy.get(header.logOutBtn)
+            .should("be.visible")
+            .click();
+        cy.get(loginPage.loginHeading).should("be.visible");
     });
 
     it("Create organization - no organization name", () => {
-        cy.get(myOrganizations.addNewOrgSpan).click();
-        cy.get(organizationModal.nextBtn).click({ force: true });
+        cy.get(myOrganizations.addNewOrgSpan)
+            .should("be.visible")
+            .click();
+        cy.get(organizationModal.nextBtn).should(($button) => {
+            expect($button).to.be.disabled
+        });
     });
 
     it("Create organization - name with all spaces", () => {
         cy.get(organizationModal.orgNameInput).type("    ");
-        cy.get(organizationModal.nextBtn).click({ force: true });
+        cy.get(organizationModal.nextBtn).should(($button) => {
+            expect($button).to.be.disabled
+        });
     });
 
     it("Create organization - valid organization name", () => {
         cy.get(organizationModal.orgNameInput).type("Cypress org");
-        cy.get(organizationModal.nextBtn).click();
-        cy.get(organizationModal.nextBtn).click();
-        cy.get(modal.okBtn).click();
+        cy.get(organizationModal.nextBtn)
+            .should("be.visible")
+            .click();
+        cy.get(organizationModal.nextBtn)
+            .should("be.visible")
+            .click();
+        cy.get(modal.okBtn)
+            .should("be.visible")
+            .click();
     });
 
     it("Update organization name - empty name field", () => {
-        cy.get(sidebar.sideMenu.organizationOptions.configurationBtn).click();
+        cy.get(sidebar.sideMenu.organizationOptions.configurationBtn)
+            .should("be.visible")
+            .click();
         cy.get(orgPage.orgNameInput).clear();
-        cy.get(orgPage.updateBtn).eq(0).click({ force: true });
+        cy.get(orgPage.validationMsgSpan)
+            .should("be.visible")
+            .and("have.text", data.validationMessages.requiredNameField);
+        cy.get(orgPage.updateBtn).eq(0).should(($button) => {
+            expect($button).to.be.disabled
+        });
     });
 
     it("Update organization name - all spaces", () => {
         cy.get(orgPage.orgNameInput).type("    ");
-        cy.get(orgPage.updateBtn).eq(0).click({ force: true });
+        cy.get(orgPage.validationMsgSpan).eq(0)
+            .should("be.visible")
+            .and("have.text", data.validationMessages.requiredNameField);
+        cy.get(orgPage.updateBtn).eq(0).should(($button) => {
+            expect($button).to.be.disabled
+        });
     });
 
     it("Update organization name - positive", () => {
         cy.get(orgPage.orgNameInput).clear().type("Edit Cypress org");
-        cy.get(orgPage.updateBtn).eq(0).click();
+        cy.get(orgPage.updateBtn).eq(0)
+            .should("be.visible")
+            .click();
+        cy.get(orgPage.validationMsgSpan).should("not.be.visible");
+        cy.get(orgPage.orgNameInput).should("have.value", "Edit Cypress org");
     });
 
     it("Delete organization - wrong password", () => {
-        cy.get(orgPage.deleteBtn).click();
+        cy.get(orgPage.deleteBtn)
+            .scrollIntoView()
+            .should("be.visible").click();
         cy.get(modal.confirmActionPassInput).type(data.invalidPassword.wrongPassword);
-        cy.get(modal.yesBtn).click();
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
+        cy.get(modal.confirmActionH4).should("be.visible");
     });
 
     it("Delete organization", () => {
         cy.get(modal.confirmActionPassInput).clear().type(data.user.password);
-        cy.get(modal.yesBtn).click();
-    });
-
-    it("Logout", () => {
-        cy.get(sidebar.mainSidebar.myAccountAvatarImg).click();
-        cy.get(sidebar.sideMenu.accountOptions.profileBtn).click();
-        cy.get(header.logOutBtn).click();
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
+        cy.get(myOrganizations.createdOrgDiv).should("not.exist");
     });
 
 });
--- cypress/integration/tasks.spec.js
+++ cypress/integration/tasks.spec.js
@@ -11,23 +11,32 @@ import sidebar from "../pages/sidebar.json";
 import header from "../pages/header.json";
 
 describe("Column and task CRUD", () => {
-    it("Visit login page and login", () => {
+    before(() => {
         cy.visit("/login");
         cy.get(loginPage.emailInput).type(data.user.email);
         cy.get(loginPage.passwordInput).type(data.user.password);
-        cy.get(loginPage.loginBtn).click();
-    });
+        cy.get(loginPage.loginBtn)
+            .should("be.visible")
+            .click();
+        cy.url().should("contain", "/my-organizations");
 
-    it("Create organization", () => {
-        cy.get(myOrgs.addNewOrgSpan).click();
+        cy.get(myOrgs.addNewOrgSpan)
+            .should("be.visible")
+            .click();
         cy.get(orgModal.orgNameInput).type("Cypress org");
-        cy.get(orgModal.nextBtn).click();
-        cy.get(orgModal.nextBtn).click();
-        cy.get(modal.okBtn).click();
-    });
+        cy.get(orgModal.nextBtn)
+            .should("be.visible")
+            .click();
+        cy.get(orgModal.nextBtn)
+            .should("be.visible")
+            .click();
+        cy.get(modal.okBtn)
+            .should("be.visible")
+            .click();
 
-    it("Create board", () => {
-        cy.get(orgPage.addNewBoardDiv).click();
+        cy.get(orgPage.addNewBoardDiv)
+            .should("be.visible")
+            .click();
         cy.get(boardModal.orgDropdownBtn).click();
         cy.get(boardModal.orgFromDropdownLi).click();
         cy.get(boardModal.boardTitleInput).type("First board");
@@ -36,76 +45,130 @@ describe("Column and task CRUD", () => {
         cy.get(boardModal.nextBtn).click();
         cy.get(boardModal.nextBtn).click();
         cy.get(boardModal.nextBtn).click();
+        cy.get(sidebar.sideMenu.boardOptions.productBacklogBtn).should("be.visible")
+    });
+
+    after(() => {
+        cy.get(sidebar.sideMenu.boardOptions.configurationBtn)
+            .should("be.visible")
+            .click();
+        cy.get(boardPage.deleteBtn)
+            .scrollIntoView()
+            .should("be.visible")
+            .click();
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
+        cy.get(modal.okBtn).click();
+
+        cy.get(sidebar.sideMenu.organizationOptions.configurationBtn)
+            .should("be.visible")
+            .click();
+        cy.get(orgPage.deleteBtn)
+            .scrollIntoView()
+            .should("be.visible")
+            .click();
+        cy.get(modal.confirmActionPassInput).clear().type(data.user.password);
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
+
+        cy.get(sidebar.mainSidebar.myAccountAvatarImg)
+            .should("be.visible")
+            .click();
+        cy.get(sidebar.sideMenu.accountOptions.profileBtn)
+            .should("be.visible")
+            .click();
+        cy.get(header.logOutBtn)
+            .should("be.visible")
+            .click();
+        cy.get(loginPage.loginHeading).should("be.visible");
     });
 
     it("Create new column", () => {
-        cy.get(boardPage.addColumnBtn).click();
+        cy.get(boardPage.addColumnBtn)
+            .should("be.visible")
+            .click();
         cy.get(boardPage.columnNameInput).type("New column{enter}");
+        cy.get(boardPage.columnNameH2).eq(1).should("be.visible");
     });
 
     it("Create task", () => {
         cy.get(boardPage.addTaskBtn).eq(1).invoke("show").click();
         cy.get(boardPage.taskTitleTextarea).type("First task{enter}");
+        cy.get(boardPage.taskCardDiv).contains("First task").should("be.visible");
     });
 
     it("Edit task title and add description", () => {
         cy.wait(1000);
-        cy.get(boardPage.taskCardDiv).eq(1).click();
+        cy.get(boardPage.taskCardDiv).contains("First task")
+            .should("be.visible")
+            .click();
         cy.get(taskModal.taskTitleH2).click();
         cy.get(taskModal.taskTitleInput).clear().type("Edit title");
-        cy.get(taskModal.saveTitleBtn).click();
-        cy.get(taskModal.editDescriptionBtn).click();
+        cy.get(taskModal.saveTitleBtn)
+            .should("be.visible")
+            .click();
+        cy.get(taskModal.editDescriptionBtn)
+            .scrollIntoView()
+            .should("be.visible")
+            .click();
         cy.get(taskModal.descriptionTextarea).type("Task description");
         cy.get(taskModal.saveDescBtn).click();
     });
 
     it("Move task to another column", () => {
-        cy.get(taskModal.sprintInfoDropdownBtn).click();
-        cy.get(taskModal.columnFromDropdownLi).eq(1).click();
+        cy.get(taskModal.sprintInfoDropdownBtn)
+            .should("be.visible")
+            .click();
+        cy.get(taskModal.columnFromDropdownLi).eq(1)
+            .should("contain.text", "New column")
+            .click();
     });
 
     it("Delete task", () => {
-        cy.get(taskModal.taskOptionsBtn).click();
-        cy.get(taskModal.deleteTaskLi).click();
-        cy.get(modal.yesBtn).click();
+        cy.get(taskModal.taskOptionsBtn)
+            .should("be.visible")
+            .click();
+        cy.get(taskModal.deleteTaskLi)
+            .should("include.text", "Delete")
+            .click();
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
     });
 
     it("Edit column name", () => {
-        cy.get(boardPage.columnNameH2).eq(1).click();
+        cy.get(boardPage.columnNameH2).eq(1)
+            .should("be.visible")
+            .click();
         cy.get(boardPage.editColumnNameInput).clear().type("Column edit{enter}")
+        cy.get(boardPage.columnNameH2).eq(1).should("have.text", "Column edit")
     });
 
     it("Delete column", () => {
-        cy.get(boardPage.columnOptionsBtn).eq(2).click();
-        cy.get(boardPage.deleteColumnLi).eq(0).click();
-        cy.get(modal.yesBtn).click();
+        cy.get(boardPage.columnOptionsBtn).eq(2)
+            .should("be.visible")
+            .click();
+        cy.get(boardPage.deleteColumnLi).eq(0)
+            .should("include.text", "Delete")
+            .click();
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
     });
 
     it("Start sprint", () => {
-        cy.get(boardPage.columnOptionsBtn).eq(1).click();
-        cy.get(boardPage.startSprintLi).eq(2).click();
+        cy.get(boardPage.columnOptionsBtn).eq(1)
+            .should("be.visible")
+            .click();
+        cy.get(boardPage.startSprintLi).eq(2)
+            .should("include.text", "Start")
+            .click();
         cy.get(modal.sprintGoalInput).type("Goals");
-        cy.get(modal.yesBtn).click();
-    });
-
-    it("Delete board", () => {
-        cy.get(sidebar.sideMenu.boardOptions.configurationBtn).click();
-        cy.get(boardPage.deleteBtn).click();
-        cy.get(modal.yesBtn).click();
-        cy.get(modal.okBtn).click();
-    });
-
-    it("Delete organization", () => {
-        cy.get(sidebar.sideMenu.organizationOptions.configurationBtn).click();
-        cy.get(orgPage.deleteBtn).click();
-        cy.get(modal.confirmActionPassInput).clear().type(data.user.password);
-        cy.get(modal.yesBtn).click();
-    });
-
-    it("Logout", () => {
-        cy.get(sidebar.mainSidebar.myAccountAvatarImg).click();
-        cy.get(sidebar.sideMenu.accountOptions.profileBtn).click();
-        cy.get(header.logOutBtn).click();
+        cy.get(modal.yesBtn)
+            .should("be.visible")
+            .click();
     });
 
 });
